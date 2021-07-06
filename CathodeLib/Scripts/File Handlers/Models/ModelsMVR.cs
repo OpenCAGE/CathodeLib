6b9a061ad6bdbc9f10c42c5f2f98e1e09a29edc4
MattFiler <matthew.filer@hotmail.co.uk>
2021-07-06 23:48:56 +0100

    Updated MVR struct

--- CathodeLib/Scripts/File Handlers/Models/ModelsMVR.cs
+++ CathodeLib/Scripts/File Handlers/Models/ModelsMVR.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Runtime.InteropServices;
+using System;
 #if UNITY_EDITOR || UNITY_STANDALONE
 using UnityEngine;
 #else
@@ -64,33 +65,41 @@ namespace CATHODE.Models
     public struct alien_mvr_entry
     {
         public Matrix4x4 Transform;
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 12)]
-        public float[] Unknowns0_; // NOTE: The 0th element is -Pi on entry 61 of 'hab_airport'.
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
-        public float[] Unknowns1_;
-        public float UnknownValue2_;
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 5)]
+        public Vector4[] InstanceState;
+        public UInt32 UnknownID; // TODO: Is this an ID or two u16s?
         public float UnknownValue3_;
-        public float UnknownValue4_; //id for something? not found in any other level files
-        public int Unknown2_;
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
-        public float[] Unknown2f_;
+        public float UnknownValue4_;
+        public Int32 Unknown2_;
+
+        public Vector4 Unknown3_;
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
-        public uint[] Unknowns2_;
+        public UInt32[] Unknowns2_;
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
         public Vector3[] UnknownMinMax_; // NOTE: Sometimes I see 'nan's here too.
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 9)]
-        public int[] Unknowns3_; // NOTE: The 9th and 11th elements seem to be incrementing indices.
-        public uint REDSIndex; // Index 45
-        public uint ModelCount;
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 5)]
-        public int[] Unknowns5_; //index 0 collision map entry?, final index is always 128?
-        public uint NodeID; // Index 52
-        public uint UnknownValue0; //another id for something?
-        public uint UnknownIndex;
-        public uint UnknownValue1;
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
-        public float[] Unknowns4_; //zeroing index 2 stop model rendering
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
-        public int[] Unknowns6_;
+        public Vector4 Zeros0_; // NOTE: Not verified by an assert. Verified by looking at MaxValues at the end of dump.
+
+        public Vector4 Zeros1_; // NOTE: Not verified by an assert. Verified by looking at MaxValues at the end of dump.
+        public UInt32 Zeros2_; // NOTE: Not verified by an assert. Verified by looking at MaxValues at the end of dump.
+        public UInt32 REDSIndex; // Index 45
+        public UInt32 ModelCount;
+        public UInt32 ResourcesBINIndex; // NOTE: This is actually 'IndexFromMVREntry' field on 'alien_resources_bin_entry'
+
+        public Vector4 Unknowns5_;
+        public UInt32 NodeID; // Index 52
+        public UInt32 ResourcesBINID; // NOTE: This is 'IDFromMVREntry' field on 'alien_resources_bin_entry'.
+        public UInt32 EnvironmentMapBINIndex; // NOTE: Tells me which Environment Map texture to use.
+        public UInt32 UnknownValue1;
+
+        public float UnknownValue;
+        public UInt32 Unknown5_;
+        public UInt32 CollisionMapThingID;
+        public UInt32 Unknowns60_;
+        public UInt32 Unknowns61_;
+        public UInt16 Unknown17_;   // TODO: It is -1 most of the time, but some times it isn't.
+        public UInt16 IsThisTypeID; // TODO: So far 3: Static, 6: Physics (Cone, Box) 7: Scripted (Door, Camera).
+        public UInt32 Unknowns70_;
+        public UInt32 Unknowns71_;
     };
 }
