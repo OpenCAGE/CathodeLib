14dbb2a5c120de63c36d3693d7cf4e9801e0ee0b
MattFiler <matthew.filer@hotmail.co.uk>
2021-10-29 18:55:47 +0100

    Changes to MVR struct

--- CathodeLib/Scripts/File Handlers/Models/ModelsMVR.cs
+++ CathodeLib/Scripts/File Handlers/Models/ModelsMVR.cs
@@ -13,63 +13,71 @@ namespace CATHODE.Models
     /* Handles CATHODE MVR files */
     public class ModelsMVR
     {
-        private string filepath;
-        private alien_mvr_header header;
-        private alien_mvr_entry[] movers;
+        private string FilePath = "";
+        private int FileSize = 32;
+        private int EntryCount = 0;
+        private int EntrySize = 320;
+        private int UnknownNumber = 0;
+
+        public List<alien_mvr_entry> Movers = new List<alien_mvr_entry>();
 
         /* Load the file */
+        public ModelsMVR() { }
         public ModelsMVR(string pathToFile)
         {
-            filepath = pathToFile;
+            FilePath = pathToFile;
 
-            BinaryReader stream = new BinaryReader(File.OpenRead(filepath));
-            header = Utilities.Consume<alien_mvr_header>(stream);
-            movers = Utilities.ConsumeArray<alien_mvr_entry>(stream, (int)header.EntryCount);
+            BinaryReader stream = new BinaryReader(File.OpenRead(FilePath));
+            FileSize = stream.ReadInt32();
+            EntryCount = stream.ReadInt32();
+            UnknownNumber = stream.ReadInt32();
+            stream.BaseStream.Position += 4;
+            EntrySize = stream.ReadInt32();
+            stream.BaseStream.Position += 12;
+            Movers = new List<alien_mvr_entry>(Utilities.ConsumeArray<alien_mvr_entry>(stream, EntryCount));
             stream.Close();
         }
 
         /* Save the file */
-        public void Save()
+        public void Save(string pathToFile = "")
         {
-            BinaryWriter stream = new BinaryWriter(File.OpenWrite(filepath));
+            if (pathToFile != "") FilePath = pathToFile;
+
+            BinaryWriter stream = new BinaryWriter(File.OpenWrite(FilePath));
             stream.BaseStream.SetLength(0);
-            Utilities.Write<alien_mvr_header>(stream, header);
-            Utilities.Write<alien_mvr_entry>(stream, movers);
+            stream.Write(FileSize);
+            stream.Write(EntryCount);
+            stream.Write(UnknownNumber);
+            stream.Write(0);
+            stream.Write(EntrySize);
+            stream.Write(0); stream.Write(0); stream.Write(0);
+            Utilities.Write<alien_mvr_entry>(stream, Movers);
             stream.Close();
-        }
-
-        /* Data accessors */
-        public int EntryCount { get { return movers.Length; } }
-        public alien_mvr_entry[] Entries { get { return movers; } }
-        public alien_mvr_entry GetEntry(int i)
-        {
-            return movers[i];
-        }
-
-        /* Data setters */
-        public void SetEntry(int i, alien_mvr_entry content)
-        {
-            movers[i] = content;
         }
     }
 
-    [StructLayout(LayoutKind.Sequential, Pack = 1)]
-    public struct alien_mvr_header
+    public enum MVREntryType : short
     {
-        public uint Unknown0_;
-        public uint EntryCount;
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)]
-        public uint[] Unknown1_; //6
-    };
+        PARTICLE_EMITTER_1 = 1,
+        LIGHT = 2,
+        STATIC_MODEL = 3,
+        PARTICLE_EMITTER_2 = 4, //these seem to usually be from AYZ/FX_LIBRARY
+        PARTICLE_EMITTER_3 = 5, //these seem to usually be from AYZ/LEVELS
+        LIGHT_AND_PHYSICS = 6,
+        DYNAMIC_MODEL = 7,
+    }
 
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
     public struct alien_mvr_entry
     {
         public Matrix4x4 Transform;
 
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 5)]
+        public Vector4 LightColour;
+        public Vector4 MaterialTint;
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
         public Vector4[] InstanceState;
-        public UInt32 UnknownID; // TODO: Is this an ID or two u16s?
+        public cGUID UnknownID; // TODO: Is this an ID or two u16s?
         public float UnknownValue3_;
         public float UnknownValue4_;
         public Int32 Unknown2_;
@@ -79,27 +87,27 @@ namespace CATHODE.Models
         public UInt32[] Unknowns2_;
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
         public Vector3[] UnknownMinMax_; // NOTE: Sometimes I see 'nan's here too.
-        public Vector4 Zeros0_; // NOTE: Not verified by an assert. Verified by looking at MaxValues at the end of dump.
 
-        public Vector4 Zeros1_; // NOTE: Not verified by an assert. Verified by looking at MaxValues at the end of dump.
-        public UInt32 Zeros2_; // NOTE: Not verified by an assert. Verified by looking at MaxValues at the end of dump.
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 36)]
+        public byte[] blankSpace;
+
         public UInt32 REDSIndex; // Index 45
         public UInt32 ModelCount;
         public UInt32 ResourcesBINIndex; // NOTE: This is actually 'IndexFromMVREntry' field on 'alien_resources_bin_entry'
 
         public Vector4 Unknowns5_;
-        public UInt32 NodeID; // Index 52
-        public UInt32 ResourcesBINID; // NOTE: This is 'IDFromMVREntry' field on 'alien_resources_bin_entry'.
+        public cGUID NodeID; // Index 52
+        public cGUID ResourcesBINID; // NOTE: This is 'IDFromMVREntry' field on 'alien_resources_bin_entry'.
         public UInt32 EnvironmentMapBINIndex; // NOTE: Tells me which Environment Map texture to use.
         public UInt32 UnknownValue1;
 
         public float UnknownValue;
         public UInt32 Unknown5_;
-        public UInt32 CollisionMapThingID;
+        public cGUID CollisionMapThingID;
         public UInt32 Unknowns60_;
         public UInt32 Unknowns61_;
         public UInt16 Unknown17_;   // TODO: It is -1 most of the time, but some times it isn't.
-        public UInt16 IsThisTypeID; // TODO: So far 3: Static, 6: Physics (Cone, Box) 7: Scripted (Door, Camera).
+        public MVREntryType IsThisTypeID; // TODO: So far 3: Static, 6: Physics (Cone, Box) 7: Scripted (Door, Camera).
         public UInt32 Unknowns70_;
         public UInt32 Unknowns71_;
     };
