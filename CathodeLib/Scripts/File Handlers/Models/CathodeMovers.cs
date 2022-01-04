b2a51f94bd016b70b1d46a562b8428821c2585ab
MattFiler <matthew.filer@hotmail.co.uk>
2022-01-04 16:32:40 +0000

    Reversing MVR data from iOS dump

--- CathodeLib/Scripts/File Handlers/Models/CathodeMovers.cs
+++ CathodeLib/Scripts/File Handlers/Models/CathodeMovers.cs
@@ -20,7 +20,7 @@ namespace CATHODE.Models
         private int entrySize = 320;
         private int nonCommandsEntries = 0;
 
-        public List<MoverEntry> Movers = new List<MoverEntry>();
+        public List<MOVER_DESCRIPTOR> Movers = new List<MOVER_DESCRIPTOR>();
 
         /* Load the file */
         public CathodeMovers() { }
@@ -37,7 +37,7 @@ namespace CATHODE.Models
             stream.BaseStream.Position += 4; 
             entrySize = stream.ReadInt32();
             stream.BaseStream.Position += 12;
-            Movers = new List<MoverEntry>(Utilities.ConsumeArray<MoverEntry>(stream, entryCount));
+            Movers = new List<MOVER_DESCRIPTOR>(Utilities.ConsumeArray<MOVER_DESCRIPTOR>(stream, entryCount));
             stream.Close();
         }
 
@@ -51,7 +51,7 @@ namespace CATHODE.Models
             nonCommandsEntries = 0;
             for (int i = 0; i < Movers.Count; i++)
             {
-                if (Movers[i].NodeID == new cGUID(0)) nonCommandsEntries++;
+                if (Movers[i].renderConstants.NodeID == new cGUID(0)) nonCommandsEntries++;
             }
 
             BinaryWriter stream = new BinaryWriter(File.OpenWrite(filePath));
@@ -62,7 +62,7 @@ namespace CATHODE.Models
             stream.Write(0);
             stream.Write(entrySize);
             stream.Write(0); stream.Write(0); stream.Write(0);
-            Utilities.Write<MoverEntry>(stream, Movers);
+            Utilities.Write<MOVER_DESCRIPTOR>(stream, Movers);
             stream.Close();
         }
     }
@@ -79,60 +79,203 @@ namespace CATHODE.Models
     }
 
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
-    public struct MoverEntry
+    public struct MOVER_DESCRIPTOR
     {
+        /*
+         
+         RenderableScene::calculate_renderable_instance_type is called on RenderableElementSet which seems to define a type (RENDERABLE_INSTANCE::Type?) 
+         It can have values 1-9, which I think are one of (as per RenderableScene::InstanceManager):
+            - RenderableCharacterInstance
+            - RenderableDynamicFXInstance
+            - RenderableDynamicTempFXInstance
+            - RenderableEnvironmentExtraInstance
+            - RenderableEnvironmentInstance
+            - RenderableFogSphereInstance
+            - RenderableLightInstance
+            - RenderableMiscInstance
+            - RenderablePlanetInstance
+         Logic is applied based on a numeric range, rather than per individual number (although maybe specific logic is applied within these ranges)
+         The ranges are:
+            - 1-9
+            - 3-9
+            - 5-9
+            - 7-9
+         These ranges are found using RenderableScene::ForEachInstanceType<min,max>
+         MVR and REDS data is used per Type, being pulled from MOVER_DESCRIPTOR and RenderableElementSet respectively
+         
+         RenderableElementSet is always paired with a MOVER_DESCRIPTOR (see RenderableScene::create_instance)
+
+         RenderableEnvironmentInstance::set_constants is what uses RENDER_CONSTANTS - look there to define that struct
+         RenderableEnvironmentInstance::set_gpu_constants is what uses GPU_CONSTANTS - look there to define that struct
+
+         RenderableScene::initialize passes MOVER_DESCRIPTOR to create_instance and defines its length as 320
+         RenderableScene::create_instance takes MOVER_DESCRIPTOR and grabs two values:
+            296: (4) uVar1 - used as first parameter in call to RenderableScene::add_new_zone
+            300: (4) uVar3 - used as second parameter in call to RenderableScene::add_new_zone
+
+         RENDERABLE_INSTANCE::TYPE values RenderableLightInstance/RenderableDynamicFxInstance/RenderableDynamicTempFXInstance/etc do this...
+            RenderableScene::InstanceManager<>::reserve_light_light_master_sets takes takes MOVER_DESCRIPTOR and grabs two values:
+               304: uint* appears if this is 0 then light or w/e isn't initialised, then some CONCAT44 operation is applied with its value, stacking with previous lights in a while loop
+            RenderableScene::InstanceManager<>::add_instance_to_light_master_set also grabs this same value (304) and checks to see if its 0, then if its less than or equal to another value.
+
+         For personal reference of offset conversions:
+            0x40 = 64
+            0xa0 = 160
+            0x10c = 268
+            0x118 = 280
+            0x130 = 304
+            0x136 = 310
+            0x140 = 320
+
+         RenderableCharacterInstance:
+            0: MATRIX_44
+            64: GPU_CONSTANTS
+            160: RENDER_CONSTANTS
+            268: uint* (visibility)
+            310: ushort* which is used for a couple things...
+               RenderableCharacterInstance + 0x34 (ushort*) - (ushort)((uVar1 & 4) << 2)
+               if ((uVar1 & 1) != 0) then RenderableCharacterInstance::activate
+
+         RenderableEnvironmentInstance:
+            0: MATRIX_44
+            64: GPU_CONSTANTS
+            160: RENDER_CONSTANTS
+            268: uint* (visibility)
+            280: undefined4* which sets RenderableEnvironmentInstance + 0xa0 as (short)*
+            310: ushort* which is used for a couple things...
+               RenderableEnvironmentInstance + 0x34 (ushort*) - (ushort)((uVar1 & 4) << 2)
+               if ((uVar1 & 1) != 0) then RenderableEnvironmentInstance::activate
+
+         RenderableDynamicTempFXInstance:
+            0: MATRIX_44
+            64: GPU_CONSTANTS
+            160: RENDER_CONSTANTS
+            268: uint* (visibility)
+            310: if ((uVar3 & 1) != 0) then RenderableDynamicFxInstance::activate
+
+         RenderableDynamicFxInstance:
+            0: MATRIX_44
+            64: GPU_CONSTANTS 
+            160: RENDER_CONSTANTS
+            268: uint* (visibility)
+            310: *something* 
+            
+         RenderableLightInstance:
+            0: MATRIX_44
+            64: GPU_CONSTANTS
+            160: RENDER_CONSTANTS
+            268: uint* (visibility)
+            310: ushort* (logic checks on this value releated to activating RenderableLightInstance and calling LIGHT_MANAGER::add_dynamic_light)
+
+         */
+
+        //Worth noting that GPU_CONSTANTS and RENDER_CONSTANTS may not actually be as big as they are here
+        //Still need to look at where their data is actually used to see the offsets
+
         public Matrix4x4 Transform;
-
-        public Vector4 LightColour;
-        public Vector4 MaterialTint;
-
-        public float lightVolumeIntensity; //todo: idk if this is right, but editing this value seems to increase/decrease the brightness of the light volume meshes
-        public float particleIntensity; //0 = black particle
-        public float particleSystemOffset; //todo: not sure entirely, but increasing this value seems to apply an offset to particle systems
-
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
-        public byte[] blankSpace1;
-
-        public float lightRadius;
-        public Vector2 textureTile; //x = horizontal tile, y = vertical tile
-
-        public float UnknownValue1_;
-        public float UnknownValue2_;
-        public float UnknownValue3_;
-        public float UnknownValue4_;
-
+        //64
+        public GPU_CONSTANTS gpuConstants;
+        //144
         public cGUID UnknownID; // TODO: Is this an ID or two u16s?
         public float UnknownValue5_;
         public float UnknownValue6_;
         public Int32 Unknown2_;
-
-        public Vector4 Unknown3_;
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
-        public UInt32[] Unknowns2_;
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
-        public Vector3[] UnknownMinMax_; // NOTE: Sometimes I see 'nan's here too.
-
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 36)]
-        public byte[] blankSpace3;
-
+        //160
+        public RENDER_CONSTANTS renderConstants;
+        //244
         public UInt32 REDSIndex; // Index 45
         public UInt32 ModelCount;
         public UInt32 ResourcesBINIndex; // NOTE: This is actually 'IndexFromMVREntry' field on 'alien_resources_bin_entry'
-
-        public Vector4 Unknowns5_;
+        //256
+        public Vector3 Unknowns5_;
+        public UInt32 Visibility; // pulled from iOS dump - should be visibility var?
+        //272
         public cGUID NodeID; // Index 52
         public cGUID ResourcesBINID; // NOTE: This is 'IDFromMVREntry' field on 'alien_resources_bin_entry'.
         public UInt32 EnvironmentMapBINIndex; // NOTE: Tells me which Environment Map texture to use.
         public UInt32 UnknownValue1;
-
+        //288
         public float UnknownValue;
         public UInt32 Unknown5_;
-        public cGUID CollisionMapThingID;
-        public UInt32 Unknowns60_;
+        //296
+        public cGUID CollisionMapThingID; //this is uVar1 in RenderableScene::create_instance
+        public UInt32 Unknowns60_; //this is uVar3 in RenderableScene::create_instance
+        //304
         public UInt32 Unknowns61_;
         public UInt16 Unknown17_;   // TODO: It is -1 most of the time, but some times it isn't.
-        public MoverType IsThisTypeID; // TODO: So far 3: Static, 6: Physics (Cone, Box) 7: Scripted (Door, Camera).
+        //310
+        public MoverType IsThisTypeID; //ushort
+        //312
         public UInt32 Unknowns70_;
         public UInt32 Unknowns71_;
+        //320
     };
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct GPU_CONSTANTS //As best I can tell, this is 80 bytes long
+    {
+        /*
+         
+        As per RenderableEnvironmentInstance::set_gpu_constants, values are at:
+        (undefined 8) 0
+        (undefined 8) 8
+        (undefined 8) 16
+        (undefined 8) 24
+        (undefined 8) 32
+        (undefined 8) 40
+        (undefined 8) 48
+        (undefined 8) 56
+        (undefined 8) 64
+        (undefined 8) 72
+         
+        */
+
+
+        //64
+        public Vector4 LightColour;
+        public Vector4 MaterialTint;
+        //96
+        public float lightVolumeIntensity; //todo: idk if this is right, but editing this value seems to increase/decrease the brightness of the light volume meshes
+        public float particleIntensity; //0 = black particle
+        public float particleSystemOffset; //todo: not sure entirely, but increasing this value seems to apply an offset to particle systems
+        //108
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
+        public byte[] blankSpace1;
+        //116
+        public float lightRadius;
+        public Vector2 textureTile; //x = horizontal tile, y = vertical tile
+        //128
+        public float UnknownValue1_;
+        public float UnknownValue2_;
+        public float UnknownValue3_;
+        public float UnknownValue4_;
+        //144
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct RENDER_CONSTANTS //appears to be 84 long
+    {
+        /*
+         
+        used in
+          RenderableEnvironmentInstance::set_constants
+          RenderableMiscInstance::set_constants 
+          RenderablePlanetInstance::set_constants
+        etc
+         
+        */
+
+        //160
+        public Vector4 Unknown3_;
+        //176
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
+        public UInt32[] Unknowns2_;
+        //184
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
+        public Vector3[] UnknownMinMax_; // NOTE: Sometimes I see 'nan's here too.
+        //208
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 36)]
+        public byte[] blankSpace3;
+        //244
+    }
 }
