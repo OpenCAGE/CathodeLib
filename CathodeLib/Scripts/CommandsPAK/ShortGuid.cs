6acf9550fc55ea79f3d96243e4367c707a0c0ace
MattFiler <matthew.filer@hotmail.co.uk>
2022-04-18 19:51:06 +0100

    minor

--- CathodeLib/Scripts/CommandsPAK/ShortGuid.cs
+++ CathodeLib/Scripts/CommandsPAK/ShortGuid.cs
@@ -8,18 +8,27 @@ using System.Text;
 
 namespace CATHODE.Commands
 {
-    public class ShortGuidUtils
+    public static class ShortGuidUtils
     {
         private static ShortGuidTable vanilla = new ShortGuidTable();
         private static ShortGuidTable custom = new ShortGuidTable();
 
+        private static CommandsPAK commandsPAK;
+
         /* Pull in strings we know are cached as ShortGuid in Cathode */
-        static ShortGuidUtils()
+        static ShortGuidUtils(/*CommandsPAK pak = null*/)
         {
+            /*
+            commandsPAK = pak;
+            commandsPAK.OnSaved += SaveCustomNames;
+            */
+
             BinaryReader reader = new BinaryReader(new MemoryStream(CathodeLib.Properties.Resources.entity_parameter_names));
             while (reader.BaseStream.Position < reader.BaseStream.Length)
                 Cache(new ShortGuid(reader), reader.ReadString(), true);
             reader.Close();
+
+            LoadCustomNames();
         }
 
         /* Generate a ShortGuid to interface with the Cathode scripting system */
@@ -54,7 +63,6 @@ namespace CATHODE.Commands
         /* Cache a pre-generated ShortGuid */
         private static void Cache(ShortGuid guid, string value, bool isVanilla = false)
         {
-            Console.WriteLine(value);
             if (isVanilla)
             {
                 if (vanilla.cache.ContainsKey(value)) return;
@@ -67,6 +75,71 @@ namespace CATHODE.Commands
                 custom.cache.Add(value, guid);
                 custom.cacheReversed.Add(guid, value);
             }
+        }
+
+        /* Pull non-vanilla ShortGuid from the CommandsPAK */
+        private static void LoadCustomNames()
+        {
+            /*
+            if (commandsPAK == null) return;
+            int endPos = GetEndOfCommands();
+            BinaryReader reader = new BinaryReader(File.OpenRead(commandsPAK.Filepath));
+            reader.BaseStream.Position = endPos;
+            if ((int)reader.BaseStream.Length - endPos == 0 || reader.ReadByte() != (byte)254)
+            {
+                custom_composites = new Dictionary<ShortGuid, Dictionary<ShortGuid, string>>();
+                reader.Close();
+                return;
+            }
+            reader.BaseStream.Position += 4;
+            int compCount = reader.ReadInt32();
+            reader.BaseStream.Position += 8;
+            custom_composites = ConsumeDatabase(reader, compCount);
+            reader.Close();
+            */
+        }
+
+        /* Write non-vanilla entity names to the CommandsPAK */
+        private static void SaveCustomNames()
+        {
+            /*
+            if (commandsPAK == null) return;
+            int endPos = GetEndOfCommands();
+            //TODO: move this writing functionality into its own thing
+            BinaryWriter writer = new BinaryWriter(File.OpenWrite(commandsPAK.Filepath));
+            bool hasAlreadyWritten = (int)writer.BaseStream.Length - endPos != 0;
+            int posToJumpBackTo = 0;
+            writer.BaseStream.Position = endPos;
+            writer.Write((byte)254);
+            if (hasAlreadyWritten)
+            {
+                //Get position here
+                writer.BaseStream.Position += 8;
+            }
+            else
+            {
+                writer.Write(0);
+                writer.Write(0);
+            }
+            posToJumpBackTo = (int)writer.BaseStream.Position;
+            writer.Write(0);
+            writer.Write(0);
+            //Jump to got position here
+            int posToWrite = (int)writer.BaseStream.Position;
+            writer.BaseStream.Position = posToJumpBackTo;
+            writer.Write(posToWrite);
+            writer.Close();
+            */
+        }
+
+        /* Get the position where we start writing our own data */
+        private static int GetEndOfCommands()
+        {
+            BinaryReader reader = new BinaryReader(File.OpenRead(commandsPAK.Filepath));
+            reader.BaseStream.Position = 20;
+            int end_of_pak = (reader.ReadInt32() * 4) + (reader.ReadInt32() * 4);
+            reader.Close();
+            return end_of_pak;
         }
 
         private class ShortGuidTable
