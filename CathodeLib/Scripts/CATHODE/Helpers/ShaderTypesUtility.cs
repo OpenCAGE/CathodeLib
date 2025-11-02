972861821b0921465d10b42d0897875c8140af9c
Matt Filer <matthew.filer@hotmail.co.uk>
2025-11-02 15:18:12 +0000

    add utility for accessing shader info

---
+++ CathodeLib/Scripts/CATHODE/Helpers/ShaderTypesUtility.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace CATHODE.ShaderTypes
+{
+    /// <summary>
+    /// Utility class for accessing shader features via reflection.
+    /// </summary>
+    public static class ShaderUtility
+    {
+        private static readonly Assembly Assembly = typeof(ShaderUtility).Assembly;
+
+        /// <summary>
+        /// Gets all enum values from the specified enum type for a shader type class.
+        /// </summary>
+        public static List<string> GetShaderFunctionality(SHADER_LIST shaderType, ShaderIndexType indexType)
+        {
+            string shaderTypeName = shaderType.ToString();
+
+            try
+            {
+                Type shaderTypeClass = Assembly.GetType($"CATHODE.ShaderTypes.{shaderTypeName}");
+                
+                if (shaderTypeClass == null)
+                    shaderTypeClass = Assembly.GetTypes().FirstOrDefault(t => t.Namespace == "CATHODE.ShaderTypes" && t.Name == shaderTypeName);
+
+                if (shaderTypeClass == null)
+                    return new List<string>();
+
+                Type enumType = shaderTypeClass.GetNestedType(indexType.ToString(), BindingFlags.Public | BindingFlags.Static);
+                if (enumType == null || !enumType.IsEnum)
+                    return new List<string>();
+
+                return Enum.GetNames(enumType).ToList();
+            }
+            catch
+            {
+                return new List<string>();
+            }
+        }
+
+        /// <summary>
+        /// Gets all samplers for the specified shader type.
+        /// </summary>
+        public static List<string> GetSamplers(SHADER_LIST shaderType)
+        {
+            return GetShaderFunctionality(shaderType, ShaderIndexType.SAMPLERS);
+        }
+
+        /// <summary>
+        /// Gets all features for the specified shader type.
+        /// </summary>
+        public static List<string> GetFeatures(SHADER_LIST shaderType)
+        {
+            return GetShaderFunctionality(shaderType, ShaderIndexType.FEATURES);
+        }
+
+        /// <summary>
+        /// Gets all parameters for the specified shader type.
+        /// </summary>
+        public static List<string> GetParameters(SHADER_LIST shaderType)
+        {
+            return GetShaderFunctionality(shaderType, ShaderIndexType.PARAMETERS);
+        }
+
+        /// <summary>
+        /// Gets the integer index value of a specific enum member for a shader type.
+        /// </summary>
+        public static int? GetShaderFunctionalityIndex(SHADER_LIST shaderType, ShaderIndexType indexType, string enumMemberName)
+        {
+            string shaderTypeName = shaderType.ToString();
+
+            try
+            {
+                Type shaderTypeClass = Assembly.GetType($"CATHODE.ShaderTypes.{shaderTypeName}");
+                
+                if (shaderTypeClass == null)
+                    shaderTypeClass = Assembly.GetTypes().FirstOrDefault(t => t.Namespace == "CATHODE.ShaderTypes" && t.Name == shaderTypeName);
+
+                if (shaderTypeClass == null)
+                    return null;
+
+                Type enumType = shaderTypeClass.GetNestedType(indexType.ToString(), BindingFlags.Public | BindingFlags.Static);
+                if (enumType == null || !enumType.IsEnum)
+                    return null;
+
+                if (!Enum.IsDefined(enumType, enumMemberName))
+                    return null;
+
+                object enumValue = Enum.Parse(enumType, enumMemberName);
+                return Convert.ToInt32(enumValue);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+
+    public enum ShaderIndexType
+    {
+        FEATURES,
+        SAMPLERS,
+        PARAMETERS
+    }
+}
+
