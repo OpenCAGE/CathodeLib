67fb009bd3feed63b7b4e0232ee14a956af7e5d4
Matt Filer <matthew.filer@hotmail.co.uk>
2026-05-03 13:45:40 +0100

    update sound utils - compress the bin

--- CathodeLib/Scripts/CATHODE/Helpers/SoundUtils.cs
+++ CathodeLib/Scripts/CATHODE/Helpers/SoundUtils.cs
@@ -2,7 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Text;
+using static CATHODE.AnimClipDB;
 #if UNITY_EDITOR || UNITY_STANDALONE
 using UnityEngine;
 #endif
@@ -16,14 +18,23 @@ namespace CathodeLib
         static SoundUtils()
         {
 #if UNITY_EDITOR || UNITY_STANDALONE
-            BinaryReader reader = new BinaryReader(File.OpenRead(Application.streamingAssetsPath + "/sound_names.bin"));
+            byte[] content = File.OpenRead(Application.streamingAssetsPath + "/sound_names.bin");
 #else
-            BinaryReader reader = new BinaryReader(new MemoryStream(CathodeLib.Properties.Resources.sound_names));
+            byte[] content = CathodeLib.Properties.Resources.sound_names;
 #endif
-            int count = reader.ReadInt32();
-            nameLookup = new Dictionary<uint, string>(count);
-            for (int i = 0; i < count; i++)
-                nameLookup.Add(reader.ReadUInt32(), Utilities.ReadString(reader));
+            using (MemoryStream stream = new MemoryStream())
+            using (GZipStream compressedStream = new GZipStream(new MemoryStream(content), CompressionMode.Decompress))
+            {
+                compressedStream.CopyTo(stream);
+                using (BinaryReader reader = new BinaryReader(stream))
+                {
+                    reader.BaseStream.Position = 0;
+                    int count = reader.ReadInt32();
+                    nameLookup = new Dictionary<uint, string>(count);
+                    for (int i = 0; i < count; i++)
+                        nameLookup.Add(reader.ReadUInt32(), Utilities.ReadString(reader));
+                }
+            }
         }
 
         public static string GetSoundName(uint id)
